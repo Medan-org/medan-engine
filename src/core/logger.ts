589c768f990744817bff2351bf1f732189ea7480
IamBobo <abdulsamad.arilewola@gmail.com>
2026-05-02 12:38:31 +0100

    Create logger.ts

---
+++ src/core/logger.ts
@@ -0,0 +1,19 @@
+export function log(message: string) {
+  console.log(`> ${message}`);
+}
+
+export function info(message: string) {
+  console.log(`ℹ️  ${message}`);
+}
+
+export function success(message: string) {
+  console.log(`✓ ${message}`);
+}
+
+export function warn(message: string) {
+  console.warn(`⚠️  ${message}`);
+}
+
+export function error(message: string) {
+  console.error(`✖ ${message}`);
+}
