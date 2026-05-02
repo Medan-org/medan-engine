bdc357b9d00b22a133d81da82fa924f03b2bb577
IamBobo <abdulsamad.arilewola@gmail.com>
2026-05-02 12:43:47 +0100

    Update index.ts

--- src/index.ts
+++ src/index.ts
@@ -4,3 +4,6 @@ export * from "./install/installer";
 export * from "./run/runner";
 export * from "./env/env";
 export * from "./core/manifest";
+export * from "./core/logger";
+export * from "./core/errors";
+export * from "./registry/registry";
